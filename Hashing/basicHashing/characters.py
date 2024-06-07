55768d04d65ad2b66687b6676d8ec521589044d2
Simran <itssimran903@gmail.com>
2024-06-08 02:27:02 +0530

    file updated

--- Hashing/basicHashing/characters.py
+++ Hashing/basicHashing/characters.py
@@ -17,3 +17,24 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# if string contains both upper and lower case characters
+# def main():
+#     # Read the input string
+#     s = input()
+
+#     # Precompute the frequency of each character in the string
+#     hash_map = [0] * 256
+#     for char in s:
+#         hash_map[ord(char)] += 1
+
+#     # Read the number of queries
+#     q = int(input())
+#     for _ in range(q):
+#         # Read the character for which the frequency is to be fetched
+#         c = input().strip()
+#         # Fetch and print the frequency from hash_map
+#         print(hash_map[ord(c)])
+
+# if __name__ == "__main__":
+#     main()
