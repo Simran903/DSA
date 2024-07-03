4c5922f3bbe03dfbd359536ccd6b8ec29ed83ac6
Simran <itssimran903@gmail.com>
2024-07-04 01:56:28 +0530

    insertion sort

--- Sorting/InsertionSort.cpp
+++ Sorting/InsertionSort.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void insertion_sort(int arr[], int n){
+void insertion_sort(int arr[], int n) {
     for (int i = 0; i <= n - 1; i++) {
         int j = i;
         while (j > 0 && arr[j - 1] > arr[j]) {
@@ -10,13 +10,21 @@ void insertion_sort(int arr[], int n){
             arr[j] = temp;
             j--;
         }
+    }
+
+    cout << "After Using insertion sort: " << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
 }
 
-int main(){
+int main()
+{
     int arr[] = {13, 46, 24, 52, 20, 9};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    cout << "Before using Insertion sort: " << endl;
-    for(int i = 0; i < n; i++)
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Before Using insertion Sort: " << endl;
+    for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
