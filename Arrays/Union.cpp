dad8636f52628fbe47c9e1344b9ad30a876d78b2
Simran <itssimran903@gmail.com>
2024-07-25 16:57:07 +0530

    Longest subarray with sum K

--- Arrays/Union.cpp
+++ Arrays/Union.cpp
@@ -1,51 +1,65 @@
-
-
-
 #include <bits/stdc++.h>
-
 using namespace std;
 
-vector < int > FindUnion(int arr1[], int arr2[], int n, int m) {
-  int i = 0, j = 0; // pointers
-  vector < int > Union; // Uninon vector
-  while (i < n && j < m) {
-    if (arr1[i] <= arr2[j]) // Case 1 and 2
+// Function to find the union of two sorted arrays
+vector<int> FindUnion(int arr1[], int arr2[], int n, int m)
+{
+  int i = 0, j = 0;  // Pointers to iterate over arr1 and arr2
+  vector<int> Union; // Vector to store the union of arr1 and arr2
+
+  // Traverse both arrays until one of them is exhausted
+  while (i < n && j < m)
+  {
+    // Case 1 and 2: If current element of arr1 is less than or equal to current element of arr2
+    if (arr1[i] <= arr2[j])
     {
+      // If the Union vector is empty or the last element in Union is not equal to arr1[i], add arr1[i]
       if (Union.size() == 0 || Union.back() != arr1[i])
         Union.push_back(arr1[i]);
       i++;
-    } else // case 3
+    }
+    else
     {
+      // Case 3: If current element of arr1 is greater than current element of arr2
+      // If the Union vector is empty or the last element in Union is not equal to arr2[j], add arr2[j]
       if (Union.size() == 0 || Union.back() != arr2[j])
         Union.push_back(arr2[j]);
       j++;
     }
   }
-  while (i < n) // IF any element left in arr1
+
+  // If there are remaining elements in arr1, add them to Union
+  while (i < n)
   {
     if (Union.back() != arr1[i])
       Union.push_back(arr1[i]);
     i++;
   }
-  while (j < m) // If any elements left in arr2
+
+  // If there are remaining elements in arr2, add them to Union
+  while (j < m)
   {
     if (Union.back() != arr2[j])
       Union.push_back(arr2[j]);
     j++;
   }
-  return Union;
+
+  return Union; // Return the union vector
 }
 
 int main()
-
 {
-  int n = 10, m = 7;
-  int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  int arr2[] = {2, 3, 4, 4, 5, 11, 12}; 
-  vector < int > Union = FindUnion(arr1, arr2, n, m);
-  cout << "Union of arr1 and arr2 is  " << endl;
-  for (auto & val: Union)
+  int n = 10, m = 7;                            // Sizes of arr1 and arr2
+  int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // First sorted array
+  int arr2[] = {2, 3, 4, 4, 5, 11, 12};         // Second sorted array
+
+  // Call FindUnion to get the union of arr1 and arr2
+  vector<int> Union = FindUnion(arr1, arr2, n, m);
+
+  // Output the result
+  cout << "Union of arr1 and arr2 is:" << endl;
+  for (auto &val : Union)
     cout << val << " ";
+
   return 0;
 }
-
